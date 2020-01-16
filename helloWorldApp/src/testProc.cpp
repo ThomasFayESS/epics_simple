e7a152a367e5cf6001b03baf7706ecafd6d226c6
=thomasfay <=thomas.fay@esss.se>
2020-01-16 20:02:23 +0100

    First working version.

---
+++ helloWorldApp/src/testProc.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <subRecord.h>
+#include <registryFunction.h>
+#include <epicsExport.h>
+
+static int testProc(subRecord *precord) {
+
+	precord->val++;
+	return 0;
+}
+
+epicsRegisterFunction(testProc);
